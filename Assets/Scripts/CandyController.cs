109b82cee20df84396e8f4a41f5f5dd73c05722c
Rusben Guzman <rusbenguzman@gmail.com>
2020-10-21 23:12:19 -0400

    declare variables for candy controller

--- Assets/Scripts/CandyController.cs
+++ Assets/Scripts/CandyController.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class CandyController : MonoBehaviour
 {
+
+    // General vars
+    private static Color _selectedColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+    private static CandyController _previusSelected = null;
+    public int id;
+
+    // Component vars
+    private SpriteRenderer _spriteRenderer;
+
+    // Aux vars
+    private bool _isSelected = false;
+    private Vector2[] adjacentDirections = new Vector2[] {
+        Vector2.up,
+        Vector2.down,
+        Vector2.left,
+        Vector2.right
+    };
+
+    void Awake() {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
