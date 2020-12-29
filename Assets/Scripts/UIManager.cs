a69ac5d36fd3cb39628aff3cf3909f37c69fd9e4
Rusben Guzman <rusbenguzman@gmail.com>
2020-12-28 22:41:04 -0500

    Create UIManager script and initialize UI texts

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text movesText;
+    private int _moveCounter;
+    private int _score;
+
+    public int Score 
+    {
+        get 
+        {
+            return _score;
+        }
+        set 
+        {
+            _score = value;
+            scoreText.text = "Score: "+_score;
+        }
+    }
+
+    public int MoveCounter 
+    {
+        get
+        {
+            return _moveCounter;
+        }
+
+        set 
+        {
+            _moveCounter = value;
+            movesText.text = "Moves: "+_moveCounter;
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _moveCounter = 30;
+        movesText.text = "Moves: "+_moveCounter;
+
+        _score = 0;
+        scoreText.text = "Score: "+_score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
